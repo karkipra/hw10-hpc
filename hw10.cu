c01191247724d095568e520f897fa193e66af0cb
Pratik Karki <karkipra@grinnell.edu>
2020-11-16 21:49:09 -0500

    f

--- hw10.cu
+++ hw10.cu
@@ -83,8 +83,8 @@ int main( int argc, char** argv) {
  
     // Initialize input array on host
     for (int i = 0; i < dimA; ++i) {
-        h_a[i] = rand();
-        h_b[i] = h_a[i];
+        h_a[i] = i; //rand();
+        //h_b[i] = h_a[i];
     }
 
     printf("h_a[0] = %d and h_b[0] = %d\n", h_a[0], h_b[0]);
@@ -105,7 +105,7 @@ int main( int argc, char** argv) {
  
     // verify the data returned to the host is correct
     for (int i = 0; i < dimA; i++){
-        //assert(h_a[i] == h_b[dimA - 1 - i]);
+        assert(h_a[i] == h_b[dimA - 1 - i]);
     }
 
     printf("dimA = %d\n", dimA-1);
