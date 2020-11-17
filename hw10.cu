8025bc22eb58e0aadc5ea8c253aeceb44dca6363
Pratik Karki <karkipra@grinnell.edu>
2020-11-17 00:14:37 -0500

    f

--- hw10.cu
+++ hw10.cu
@@ -1,65 +1,114 @@
+// includes, system
 #include <stdio.h>
 #include <assert.h>
-#include <time.h>
-
-// kernel – reverse an array A
-__global__ void reverseArray(int *A, int dim_a) {
-    int tid, temp; 
-    tid = blockIdx.x* blockDim.x+ threadIdx.x; 
-    //printf("tid = %d\n", tid);
-
-    A[tid] = tid;
-    //printf("A[%d] = %d\n", tid, A[tid]);
-
-    /*
-    if(tid < dim_a/2){
-        temp = A[dim_a-tid-1];
-        A[dim_a-tid-1] = A[tid];
-        A[tid] = temp; 
-    }*/
-} 
-
-int main() {
-    //pointer for host memory and device memory 
-    int *h_a, *d_a;
-    
-    // define thread hierarchy 
-    int dim_a= 16*1024*1024;
-    int num_blocks = 8; 
-    int num_th_per_blk= dim_a/num_blocks; 
-    
-    // allocate host and device memory 
-    size_t memSize; 
-    memSize = num_blocks* num_th_per_blk* sizeof(int); 
-    h_a = (int*) malloc(memSize);
-    cudaMalloc( (void**) &d_a, memSize); 
-
+ 
+// Simple utility function to check for CUDA runtime errors
+void checkCUDAError(const char* msg);
+ 
+// Part 2 of 2: implement the fast kernel using shared memory
+__global__ void reverseArrayBlock(int *d_out, int *d_in) {
+    extern __shared__ int s_data[];
+ 
+    int inOffset  = blockDim.x * blockIdx.x;
+    int in  = inOffset + threadIdx.x;
+ 
+    // Load one element per thread from device memory and store it 
+    // *in reversed order* into temporary shared memory
+    s_data[blockDim.x - 1 - threadIdx.x] = d_in[in];
+ 
+    // Block until all threads in the block have written their data to shared mem
+    __syncthreads();
+ 
+    // write the data from shared memory in forward order, 
+    // but to the reversed block offset as before
+ 
+    int outOffset = blockDim.x * (gridDim.x - 1 - blockIdx.x);
+ 
+    int out = outOffset + threadIdx.x;
+    d_out[out] = s_data[threadIdx.x];
+}
+ 
+////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////
+int main( int argc, char** argv) {
+    // pointer for host memory and size
+    int *h_a, *h_b;
+    int dimA = 256 * 1024; // 256K elements (1MB total)
+ 
+    // pointer for device memory
+    int *d_b, *d_a;
+ 
+    // define grid and block size
+    int numThreadsPerBlock = 256;
+ 
+    // Compute number of blocks needed based on array size and desired block size
+    int numBlocks = dimA / numThreadsPerBlock;  
+ 
+    // Part 1 of 2: Compute the number of bytes of shared memory needed
+    // This is used in the kernel invocation below
+    int sharedMemSize = numThreadsPerBlock * sizeof(int);
+ 
+    // allocate host and device memory
+    size_t memSize = numBlocks * numThreadsPerBlock * sizeof(int);
+    h_a = (int *) malloc(memSize);
+    cudaMalloc( (void **) &d_a, memSize );
+    cudaMalloc( (void **) &d_b, memSize );
+ 
     // Initialize input array on host
-    for (int i = 0; i < dim_a; ++i) {
-        //h_a[i] = i; //rand();
+    for (int i = 0; i < dimA; ++i){
+        h_a[i] = rand();
+        h_b[i] = h_a[i];
     }
-
-    // store to device
-    cudaMemcpy(d_a, h_a, memSize, cudaMemcpyDeviceToHost); 
-    
+ 
+    // Copy host array to device array
+    cudaMemcpy( d_a, h_a, memSize, cudaMemcpyHostToDevice );
+ 
     // launch kernel
-    dim3 dimGrid(num_blocks);
-    dim3 dimBlock(num_th_per_blk); 
-    reverseArray<<<dimGrid, dimBlock>>>(d_a, dim_a); 
-    
-    // TODO retrieve results 
-    cudaMemcpy(h_a, d_a, memSize, cudaMemcpyDeviceToHost); 
-
-    // Initialize input array on host
-    for (int i = 0; i < dim_a; ++i) {
-        if(i == 15) break;
-        printf("h_a[%d] = %d and (dim_a - 1 - %d) = %d\n", i, h_a[i], i,  dim_a - 1 - i);
+    dim3 dimGrid(numBlocks);
+    dim3 dimBlock(numThreadsPerBlock);
+    reverseArrayBlock<<< dimGrid, dimBlock, sharedMemSize >>>( d_b, d_a );
+ 
+    // block until the device has completed
+    cudaThreadSynchronize();
+ 
+    // check if kernel execution generated an error
+    // Check for any CUDA errors
+    checkCUDAError("kernel invocation");
+ 
+    // device to host copy
+    cudaMemcpy( h_a, d_b, memSize, cudaMemcpyDeviceToHost );
+ 
+    // Check for any CUDA errors
+    checkCUDAError("memcpy");
+ 
+    // verify the data returned to the host is correct
+    for (int i = 0; i < dimA; i++)
+    {
+        assert(h_a[i] ==  h_b[dimA-1-i]);
     }
-
+ 
     // free device memory
     cudaFree(d_a);
+    cudaFree(d_b);
+ 
     // free host memory
     free(h_a);
+    free(h_b);
+ 
+    // If the program makes it this far, then the results are correct and
+    // there are no run-time errors.  Good work!
+    printf("Correct!\n");
  
     return 0;
+}
+ 
+void checkCUDAError(const char *msg)
+{
+    cudaError_t err = cudaGetLastError();
+    if( cudaSuccess != err) 
+    {
+        fprintf(stderr, "Cuda error: %s: %s.\n", msg, cudaGetErrorString( err) );
+        exit(EXIT_FAILURE);
+    }                         
 }
