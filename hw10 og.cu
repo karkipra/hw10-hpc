b24dd07ca5c499a35f2ef81895754a9cb4455040
Pratik Karki <karkipra@grinnell.edu>
2020-11-17 12:00:56 -0500

    f

--- hw10 og.cu
+++ hw10 og.cu
@@ -10,16 +10,27 @@
     @params
     @return void
 */
-__global__ void reverseArray(int *A, int dimA){
+__global__ void reverseArray(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
 
-    int tid, temp;
-    tid = blockIdx.x* blockDim.x+ threadIdx.x; 
+    int inOffset  = blockDim.x * blockIdx.x;
+    int in  = inOffset + threadIdx.x;
 
-    if(tid < dimA/2){
-        temp = A[tid];
-        A[tid] = A[dimA-1-tid];
-        A[dimA-1-tid] = temp;
-    }
+    // Load one element per thread from device memory and store it
+    // in reversed order into temporary shared memory
+    s_data[blockDim.x - 1 - threadIdx.x] = d_in[in];
+
+    // Block until all threads in the block have written their data to shared mem
+    __syncthreads();
+
+    // write the data from shared memory in forward order,
+    // but to the reversed block offset as before
+
+    int outOffset = blockDim.x * (gridDim.x - 1 - blockIdx.x);
+
+    int out = outOffset + threadIdx.x;
+    d_out[out] = s_data[threadIdx.x];
 }
 
 /*
@@ -43,6 +54,10 @@ int main(int argc, char** argv)
     // Compute number of blocks needed based on array size and desired block size
     int numBlocks = dimA / numThreadsPerBlock;
 
+    // Part 1 of 2: Compute the number of bytes of shared memory needed
+    // This is used in the kernel invocation below
+    int sharedMemSize = numThreadsPerBlock * sizeof(int);
+
     // allocate host and device memory
     size_t memSize = numBlocks * numThreadsPerBlock * sizeof(int);
     h_a = (int *) malloc(memSize);
@@ -62,18 +77,18 @@ int main(int argc, char** argv)
     }
 
     // Copy host array to device array
-    cudaMemcpy(d_a, h_a, memSize, cudaMemcpyHostToDevice );
+    cudaMemcpy( d_a, h_a, memSize, cudaMemcpyHostToDevice );
 
     // launch kernel
     dim3 dimGrid(numBlocks);
     dim3 dimBlock(numThreadsPerBlock);
-    reverseArray<<< dimGrid, dimBlock >>>(d_a, dimA);
+    reverseArray<<< dimGrid, dimBlock, sharedMemSize >>>(d_b, d_a);
 
     // block until the device has completed
     cudaThreadSynchronize();
 
     // device to host copy
-    cudaMemcpy(h_a, d_a, memSize, cudaMemcpyDeviceToHost );
+    cudaMemcpy( h_a, d_b, memSize, cudaMemcpyDeviceToHost );
 
     printf("Verifying program correctness.... ");
     // verify the data returned to the host is correct
